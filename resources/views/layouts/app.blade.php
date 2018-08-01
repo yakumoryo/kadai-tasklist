04f04492636c38475d700547c909b2e9e60ae59f
EC2 Default User <ec2-user@ip-172-31-27-123.us-east-2.compute.internal>
2018-08-01 01:39:53 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,23 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>Tasklist</title>
+        
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
     </head>
 
     <body>
+        @include('commons.navbar')
+        @include('commons.error_messages')
+        
         @yield('content')
     </body>
 </html>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
 
-    <h1>タスク新規作成ページ</h1>
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-        
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
+　　<div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            
+            <h1>タスク新規作成ページ</h1>
+            
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content',null,['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status',null,['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿') !!}
+                {!! Form::close() !!}
+                
+        </div>
+    </div>
 @endsection
